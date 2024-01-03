1e60dc920a08c3ee69cffb558833a336dc7ae98e
laura ajala <lauraq2000@hotmail.com>
2024-01-03 16:41:07 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>Pizzaria da laura &copy; 2021</p>
+    <p>Pizzaria da Laura &copy; 2021</p>
   </footer>
   <!-- Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
