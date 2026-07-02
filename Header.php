c07042f058a18af5b1c3f44c9893540dfa0c2abe
Laura Ajala <lauraq2000@hotmail.com>
2026-07-02 11:10:29 -0400

    Update Header.php

--- Header.php
+++ Header.php
@@ -1,51 +1,55 @@
-<?php 
+<?php
+session_start();
+require_once("process/conn.php");
 
-  include("process/conn.php");
+$msg = $_SESSION["msg"] ?? "";
+$status = $_SESSION["status"] ?? "";
 
-  $msg = "";
-
-  if(isset($_SESSION["msg"])) {
-
-    $msg = $_SESSION["msg"];
-    $status = $_SESSION["status"];
-
-    $_SESSION["msg"] = "";
-    $_SESSION["status"] = "";
-
-  }
-
+// limpa após exibir (boa prática)
+unset($_SESSION["msg"], $_SESSION["status"]);
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Faça seu Pedido!</title>
+
+  <title>Pizzaria do João 🍕</title>
+
   <!-- Bootstrap -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-  <!-- App CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+  <!-- CSS próprio -->
   <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
-  <header>
-    <nav class="navbar navbar-expand-lg">
-      <a href="index.php" class="navbar-brand">
-        <img src="img/pizza.svg" alt="Pizzaria do João" id="brand-logo">
-      </a>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a href="index.php" class="nav-link">Peça sua pizza</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-  <?php if($msg != ""): ?>
-    <div class="alert alert-<?= $status ?>">
-      <p><?= $msg ?></p>
+
+<header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+    <a class="navbar-brand" href="index.php">
+      🍕 Pizzaria do João
+    </a>
+
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.php">Fazer pedido</a>
+        </li>
+      </ul>
     </div>
-  <?php endif; ?>
+
+  </nav>
+</header>
+
+<?php if (!empty($msg)): ?>
+  <div class="alert alert-<?= $status ?>">
+    <?= htmlspecialchars($msg) ?>
+  </div>
+<?php endif; ?>
